f852d8e5176a4b782cb6426ee32450175a28f46f
Sharath S Bhargav <shrthsbhargav96@gmail.com>
2020-02-16 15:58:41 +0530

    Skeleton API completed

--- web/app.py
+++ web/app.py
@@ -1,33 +1,97 @@
-from flask import Flask, jsonify, render_template, flash, request, redirect, url_for
+from flask import Flask, jsonify, render_template, flash, request, redirect, url_for,url_for, send_from_directory
+from werkzeug.utils import secure_filename
+import os
+
+import base64
+from fastai.vision import load_learner,open_image
 app = Flask(__name__)
-
-UPLOAD_FOLDER = '/path/to/the/uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.secret_key = os.urandom(24)
+UPLOAD_FOLDER = 'static/images'
+ALLOWED_EXTENSIONS = { 'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
+app.config['MODEL_PATH']='static/models'
 
-@app.route('/todo/api/v1/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+class InvalidImageException(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return self.message
 
-@app.route('/',methods=['GET'])
+def save_image(request):
+    if 'file' not in request.files:
+            #flash('No file part')
+        print("No file part")
+        raise InvalidImageException("No file part")
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        #flash('No selected file')
+        raise InvalidImageException("Invalid file name")
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        print(filename)
+        return filename
+
+@app.route('/',methods=['GET','POST'])
 def index():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        try:
+            filename = save_image(request)
+            return redirect(url_for('uploaded_file',filename=filename))
+        except InvalidImageException :
+            
+            return render_template(request.url)
+            
+    print("index")
     return render_template('index.html')
+
+  
+
+def get_predictions(filename):
+    model = load_learner(app.config['MODEL_PATH'],file='exportV1.pkl')
+    img = open_image(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    pred_class,idx,outputs = model.predict(img)
+    mapped=sorted(set(zip(outputs, model.data.classes)),reverse=True)
+
+    #os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return {'class':str(pred_class),'detail':str(mapped)}
+
+
+@app.route('/v1/results/<filename>',methods=['GET'])
+def uploaded_file(filename):
+    if request.method == "GET":
+        if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+            output = get_predictions(filename)
+            return render_template('uploaded_file.html',filename=filename,pred_class=output['class'])
+
+@app.route('/v1/results',methods=['POST'])
+def image_api():
+    print("iamge api")
+    if request.method == "POST":
+        try:
+            filename = save_image(request)
+            results = get_predictions(filename)
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            
+            print((jsonify(results)))
+            return jsonify( results)
+        except InvalidImageException as e:
+            print(e.message)
+            return jsonify({"error":e.message})
+
+
+@app.route('/clean_up/<filename>')
+def clean_up(filename):
+    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host= '0.0.0.0',debug=True,port='5000')
